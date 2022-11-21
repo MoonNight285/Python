dca3c92f68ec277e8ec69e3cb64b95791c15da0b
MoonNight285 <kimjyjh11@naver.com>
2022-11-21 14:44:08 +0900

    사칙 연산 예제

---
+++ ch02/ex1.py
@@ -0,0 +1,13 @@
+# ex1.py
+# 사칙연산
+
+a = 100
+b = 50
+result = a + b
+print(a, " + ", b, " = ", result)
+result = a - b
+print(a, ' - ', b, ' = ', result)
+result = a * b
+print(a, " * ", b, " = ", result)
+result = a / b
+print(a, " / ", b, " = ", result)
