5bdcdfc388235db0169c72f8745628ff18702ea6
MoonNight285 <kimjyjh11@naver.com>
2022-11-21 14:44:33 +0900

    import 사용 예제

---
+++ ch02/ex4.py
@@ -0,0 +1,17 @@
+# ex4
+# import 사용 예제
+
+import turtle
+
+# turtle.shape("turtle")
+# turtle.shape("classic")
+turtle.shape("triangle")
+turtle.forward(200)
+turtle.right(90)
+
+turtle.forward(200)
+turtle.right(90)
+
+turtle.forward(200)
+turtle.right(90)
+turtle.done()
