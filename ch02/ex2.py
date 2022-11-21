7238a83adeab5d27624ed1c58060f0f6396d9d5d
MoonNight285 <kimjyjh11@naver.com>
2022-11-21 14:44:19 +0900

    사칙 연산(키보드 입력) 예제

---
+++ ch02/ex2.py
@@ -0,0 +1,14 @@
+# ex2
+# 키보드 입력
+
+# 기본적으로는 문자열로 받기때문에 정수형으로 변환
+a = int(input("첫번째 숫자를 입력하세요 : "))
+b = int(input("두번째 숫자를 입력하세요 : "))
+result = a + b
+print(a, " + ", b, " = ", result)
+result = a - b
+print(a, ' - ', b, ' = ', result)
+result = a * b
+print(a, " * ", b, " = ", result)
+result = a / b
+print(a, " / ", b, " = ", result)
